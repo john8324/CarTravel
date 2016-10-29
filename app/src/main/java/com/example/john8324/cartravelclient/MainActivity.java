fc7ac2b12c43a241727c7f2bf5e8d3ddf28af3af
john8324 <ay12345678@gmail.com>
2016-10-29 13:57:45 +0800

    rewrite readCsv(), ONLY read 20 lines to avoid OutOfMemory

--- app/src/main/java/com/example/john8324/cartravelclient/MainActivity.java
+++ app/src/main/java/com/example/john8324/cartravelclient/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.*;
 
 public class MainActivity extends Activity {
 
@@ -16,35 +17,43 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         assetManager = getAssets();
+        ArrayList<String[]> csv_data = null;
 
         try {
-            ArrayList<HashMap<String, String>> csv_data = readCsv("test.csv");
+            csv_data = readCsv("test.csv");
             Log.d("", "" + csv_data.toString());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
             Log.d("", "IO Fail");
         }
 
+        TextView textView = (TextView)findViewById(R.id.textView);
+        if (csv_data == null) {
+            textView.setText("FAILLLLLLLL");
+            return;
+        }
 
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        String[] key = csv_data.get(0), value = csv_data.get(3);
+        for (int i = 0; i < value.length; i++) {
+            arrayAdapter.add(key[i] + ": " + value[i]);
+        }
+        spinner.setAdapter(arrayAdapter);
 
     }
 
-    public ArrayList<HashMap<String, String>> readCsv(String filename) throws FileNotFoundException, IOException {
-        ArrayList<HashMap<String, String>> allData = new ArrayList<>();
+    public ArrayList<String[]> readCsv(String filename) throws IOException {
+        ArrayList<String[]> allData = new ArrayList<>();
         Scanner input = new Scanner(assetManager.open(filename));
-        String[] title = input.nextLine().split(",");
-        for (int i = 0; i < title.length; i++) {
-            title[i] = title[i].trim();
-        }
-        while (input.hasNext()) {
-            String[] line = input.nextLine().split(",");
-            HashMap<String, String> aRow = new HashMap<>();
-            for (int i = 0; i < title.length; i++) {
-                aRow.put(title[i], line[i].trim());
+        while (input.hasNextLine() && allData.size() < 20) {
+            String[] row = input.nextLine().split(",");
+            // Log.d("", "Len = " + cell.length);
+            if (row.length == 1 && row[0].startsWith("#")) {
+                continue;
             }
-            allData.add(aRow);
+
+            allData.add(row);
         }
         input.close();
         return allData;
